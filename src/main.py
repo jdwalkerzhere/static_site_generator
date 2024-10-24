3eedefae4a13bd61c30933bb4b584bdbda4c469c
jdwalkerzhere <jdwalkerzhere@comcast.net>
2024-10-24 07:31:35 -0400

    Added test shell script, test file to src, and emptied main.py

--- src/main.py
+++ src/main.py
@@ -2,17 +2,7 @@ from textnode import TextNode, TextType
 
 
 def main():
-    bold = TextNode('hi there', TextType.BOLD)
-    link = TextNode('click here', TextType.LINK, 'https://boot.dev')
-    italic = TextNode('important', TextType.ITALIC)
-    italic_copy = TextNode('important', TextType.ITALIC) 
-    italic_diff = TextNode('not important', TextType.ITALIC)
-
-    print(bold)
-    print(link)
-    print(italic)
-    print(italic == italic_copy)
-    print(italic != italic_diff)
+    pass
 
 
 if __name__ == '__main__':
---
+++ src/test_textnode.py
@@ -0,0 +1,27 @@
+import unittest
+
+from textnode import TextNode, TextType
+
+
+class TestTextNode(unittest.TestCase):
+    def test_eq(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.BOLD)
+        self.assertEqual(node, node2)
+
+    def test_non_eq(self):
+        node = TextNode("This is a node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.BOLD)
+        self.assertNotEqual(node, node2)
+
+    def test_non_eq_type(self):
+        node = TextNode("This is a node", TextType.BOLD)
+        node2 = TextNode("This is a node", TextType.ITALIC)
+        self.assertNotEqual(node, node2)
+
+    def test_url(self):
+        node = TextNode("This has a url", TextType.LINK, "https://boot.dev")
+        self.assertIn(node.url, vars(node).values())
+
+if __name__ == "__main__":
+    unittest.main()
