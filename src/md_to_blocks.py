17a3fb54b18f580455902fe8b274ab5acb7ddb18
jdwalkerzhere <jdwalkerzhere@comcast.net>
2024-10-29 13:08:59 -0400

    Added block typing

--- src/md_to_blocks.py
+++ src/md_to_blocks.py
@@ -1,3 +1,5 @@
+import re
+
 from typing import List
 
 
@@ -14,3 +16,18 @@ def markdown_to_blocks(markdown) -> List[str]:
         curr_block.append(line)
 
     return blocks
+
+block_spec = [
+    ('HEADING', r'^#{1,6} .*?'),
+    ('CODE', r'^```[\s\S]*?```'),
+    ('QUOTE', r'^> .*(?:\n> .*)*\n'),
+    ('UNORDERED_LIST', r'^(?:[*|-] .*\n)+'),
+    ('ORDERED_LIST', r'^(?:\d+\. .*\n)+')
+]
+
+def block_to_block_type(block: str) -> str | None:
+    block_regex = '|'.join('(?P<%s>%s)' % pair for pair in block_spec)
+    regex_match = re.match(block_regex, block, re.DOTALL)
+    if not regex_match:
+        return 'PARAGRAPH'
+    return regex_match.lastgroup
--- src/test_md_to_blocks.py
+++ src/test_md_to_blocks.py
@@ -1,6 +1,6 @@
 import unittest
 
-from md_to_blocks import markdown_to_blocks
+from md_to_blocks import block_to_block_type, markdown_to_blocks
 
 class TestMDtoBlocks(unittest.TestCase):
     def test_md_to_blocks(self):
@@ -17,3 +17,34 @@ This is a paragraph of text. It has some **bold** and *italic* words inside of i
                            'This is a paragraph of text. It has some **bold** and *italic* words inside of it.',
                            '* This is the first list item in a list block\n* This is a list item\n* This is another list item']
         self.assertEqual(blocked_md, expected_output)
+
+        block_types = [block_to_block_type(block) for block in blocked_md]
+        expected_output = ['HEADING', 'PARAGRAPH', 'UNORDERED_LIST']
+
+        for block_type, actual_type in zip(block_types, expected_output):
+            self.assertEqual(block_type, actual_type)
+
+    def test_md_block_types(self):
+        paragraph = 'im just # text'
+        para_type = block_to_block_type(paragraph)
+        self.assertEqual(para_type, 'PARAGRAPH')
+
+        heading = '#### im a heading'
+        heading_type = block_to_block_type(heading)
+        self.assertEqual(heading_type, 'HEADING')
+
+        code = '```import re\ntext="stuff *is* weird"\nre.match(r".", text)```'
+        code_type = block_to_block_type(code)
+        self.assertEqual(code_type, 'CODE')
+
+        quote = '> hi there\n> im a quote'
+        quote_type = block_to_block_type(quote)
+        self.assertEqual(quote_type, 'QUOTE')
+
+        unordered = '* thing\n- another thing\n* another thing'
+        unordered_list_type = block_to_block_type(unordered)
+        self.assertEqual(unordered_list_type, 'UNORDERED_LIST')
+
+        ordered = '1. thing\n2. another thing\n3. another thing'
+        ordered_list_type = block_to_block_type(ordered)
+        self.assertEqual(ordered_list_type, 'ORDERED_LIST')
