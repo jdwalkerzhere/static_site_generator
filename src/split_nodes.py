411235c14d2777d0aef5bead91124ac37980a1c8
jdwalkerzhere <jdwalkerzhere@comcast.net>
2024-10-29 09:42:57 -0400

    finished inline parsing

--- src/split_nodes.py
+++ src/split_nodes.py
@@ -1,4 +1,6 @@
-from typing import List
+import re
+
+from typing import List, Tuple
 from textnode import TextNode, TextType
 
 
@@ -34,5 +36,90 @@ def split_nodes(
                 new_nodes.append(TextNode(text=text, type=TextType.TEXT))
                 continue
             new_nodes.append(TextNode(text=text, type=text_type))
+
+    return new_nodes
+
+
+def extract_images(text: str) -> List[Tuple[str,str]]:
+    return re.findall(pattern=r'!\[(.*?)\]\((.*?)\)', string=text)
+
+
+def extract_links(text: str) -> List[Tuple[str,str]]:
+    return re.findall(pattern=r'(?<!\!)\[(.*?)\]\((.*?)\)', string=text)
+
+
+def split_images(nodes: List[TextNode]) -> List[TextNode]:
+    new_nodes = []
+
+    for node in nodes:
+        extracted_images = extract_images(node.text)[::-1]
+
+        if not extracted_images:
+            new_nodes.append(node)
+            continue
+
+        i, j = 0, 2
+
+        start = 0
+
+        while j < len(node.text):
+            if node.text[i:j] == '![':
+                if abs(start - i) != 0:
+                    new_nodes.append(TextNode(text=node.text[start:i], type=TextType.TEXT))
+                alt_text, url = extracted_images.pop()
+                new_nodes.append(TextNode(text=alt_text, type=TextType.IMG, url=url))
+                i += 5 + len(alt_text) + len(url)
+                j = i + 2
+                start = i
+                continue
+            i += 1
+            j += 1
+
+        if start < len(node.text):
+            new_nodes.append(TextNode(text=node.text[start:], type=TextType.TEXT))
+
+    return new_nodes
+
+
+def split_links(nodes: List[TextNode]) -> List[TextNode]:
+    new_nodes = []
+
+    for node in nodes:
+        extracted_links = extract_links(node.text)[::-1]
+
+        if not extracted_links or node.text_type != 'text':
+            new_nodes.append(node)
+            continue
+
+        hyperlink, url = extracted_links.pop()
+
+        str_link = f'[{hyperlink}]({url})'
+
+        i = 0
+        j = len(str_link)
+
+        start = 0
+
+
+        while j <= len(node.text):
+            if node.text[i:j] == str_link:
+                if abs(start - i) != 0:
+                    new_nodes.append(TextNode(text=node.text[start:i], type=TextType.TEXT))
+                new_nodes.append(TextNode(text=hyperlink, type=TextType.LINK, url=url))
+                i += len(str_link) 
+                start = i
+                if not extracted_links:
+                    break
+
+                hyperlink, url = extracted_links.pop()
+                str_link = f'[{hyperlink}]({url})'
+
+                j = i + len(str_link)
+                continue
+            i += 1
+            j += 1
+
+        if start < len(node.text):
+            new_nodes.append(TextNode(text=node.text[start:], type=TextType.TEXT))
 
     return new_nodes
